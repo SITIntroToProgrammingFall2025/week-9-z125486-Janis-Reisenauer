7d8e0e78f6c34a5af63e704cd4cab0576cc0d1ac
z125486-Janis-Reisenauer <z125486@shibaura-it.ac.jp>
2025-12-09 15:15:53 +0900

    Add files via upload

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a[2][2];
+    int i, j;
+    double det;
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
 
+det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+printf("Determinant: %.6f\n", det);
+return 0;
+}
--- hw9-3.c
+++ hw9-3.c
@@ -1,1 +1,77 @@
+#include <stdio.h>
 
+int main()
+{
+
+    float a[2][3];
+    float b[3][2];
+    float c[2][2];
+    int i, j, k;
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%f", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%f", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            c[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            for (k = 0; k < 3; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("The first matrix you entered is:\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%.2f ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The second matrix you entered is:\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%.2f ", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The multiplication product of matrix A and matrix B :\n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%.2f ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
