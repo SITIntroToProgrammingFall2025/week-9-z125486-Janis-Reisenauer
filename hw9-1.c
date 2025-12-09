621672e7d7e10ba52a81212ffbb6a2a57715a669
z125486-Janis-Reisenauer <z125486@shibaura-it.ac.jp>
2025-12-09 15:37:25 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int a[2][2];
     int i, j;
     double det;
-    for(i=0;i<2;i++){
-        for(j=0;j<2;j++){
-            scanf("%d",&a[i][j]);
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%d", &a[i][j]);
         }
     }
 
-det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
-printf("Determinant: %.6f\n", det);
-return 0;
+    det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+    printf("Determinant is %.6f\n", det);
+    return 0;
 }
